b7fe4457edd830e9f4537a9e8b0cac98c73fff50
Jeeva Raghavan <raghavanofficials@gmail.com>
2025-07-15 02:40:36 +0000

    header logo is
    https://firebasestorage.googleapis.com/v0/b/egspec-websi

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -49,14 +49,14 @@ export function Header() {
         <div className="mr-4 flex items-center">
           <Link href="/" className="flex items-center gap-4">
              <Image
-              src="https://storage.googleapis.com/stey-prod-public/deletable/egs-logo.png"
+              src="https://firebasestorage.googleapis.com/v0/b/egspec-website.appspot.com/o/egspec%2Fenergy-2025%2Fenergy-egspgoi-logo.png?alt=media&token=b401f7dd-c3ed-4a30-84b7-8222ba965250"
               alt="EGS Pillay Group of Institutions Logo"
               width={150}
               height={40}
               className="h-10 w-auto"
             />
             <Image
-              src="https://storage.googleapis.com/stey-prod-public/deletable/energy-logo-text.png"
+              src="https://firebasestorage.googleapis.com/v0/b/egspec-website.appspot.com/o/egspec%2Fenergy-2025%2Fenergy-logo.png?alt=media&token=49e75a63-950b-4ed2-a0f7-075ba54ace2e"
               alt="Energy 2025 Inter-School Sports Meet Logo"
               width={150}
               height={40}
